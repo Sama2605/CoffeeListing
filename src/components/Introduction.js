6b6a86c50e8d7efe9fa224a4c3a4103bff6143bb
Sama2605 <malagosia526@gmail.com>
2025-02-12 21:58:58 +0100

    Page optimalization fixes

---
+++ src/components/Introduction.js
@@ -0,0 +1,16 @@
+const Inroduction = () => {
+  return (
+    <div className="heading_container">
+      <h1 className="list_heading">Our Collection</h1>
+      <p className="list_description">
+        Introducing our Coffee Collection, a selection of unique coffees
+        <br />
+        from different roast types and origins, expertly roasted in small
+        <br />
+        batches and shipped fresh weekly.
+      </p>
+    </div>
+  );
+};
+
+export default Inroduction;
